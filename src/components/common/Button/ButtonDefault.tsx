64f9cec805948710fb79a9308f6d231ea3e0f587
chaeyoon <codbs0722@gmail.com>
2026-01-07 16:55:55 +0900

    #3 [UI] ButtonMedium 구현 + [MOD] button 공통 타입 작성

--- src/components/common/Button/ButtonDefault.tsx
+++ src/components/common/Button/ButtonDefault.tsx
@@ -1,10 +1,6 @@
-type ButtonDefaultProps = {
-  text: string;
-  onClick?: () => void;
-  className?: string;
-};
+import type { ButtonProps } from '../../../types/button';
 
-const ButtonDefault = ({ text, onClick, className }: ButtonDefaultProps) => {
+const ButtonDefault = ({ text, onClick, className }: ButtonProps) => {
   return (
     <button
       className={`w-[311px] h-[48px] rounded-md font-body text-body-4 cursor-pointer ${className}`}
--- src/components/common/Button/ButtonLarge.tsx
+++ src/components/common/Button/ButtonLarge.tsx
@@ -1,14 +1,12 @@
-type ButtonLargeProps = {
-  text: string;
-};
+import type { ButtonProps } from '../../../types/button';
 
-const ButtonLarge = ({ text }: ButtonLargeProps) => {
+const ButtonLarge = ({ text }: ButtonProps) => {
   return (
-    <div className="w-[335px] h-[74px] rounded-md bg-white shadow-[0_0_9.9px_0_rgba(141,141,141,0.24)]">
+    <button className="w-[335px] h-[74px] rounded-md bg-white shadow-[0_0_9.9px_0_rgba(141,141,141,0.24)]">
       <p className="text-black-2 font-body text-body-2 py-[28px] px-[77px]">
         {text}
       </p>
-    </div>
+    </button>
   );
 };
 
