a5f047c2f37ca4b4150c8d089a3e782cb8e7fbac
chaeyoon <codbs0722@gmail.com>
2026-01-07 19:40:37 +0900

    #3 [MOD] SideBarTest UI 수정

--- src/components/common/SideBarTest.tsx
+++ src/components/common/SideBarTest.tsx
@@ -1,22 +1,27 @@
 import teatImg from '../../assets/tea.svg';
+import ButtonMedium from './Button/ButtonMedium';
 
 const SideBarTest = () => {
   return (
     <>
       <div className="w-[335px] pt-[16px] pb-[12px] px-[12px] bg-[#efe9ff] rounded-md">
         <div className="flex flex-col gap-[8px]">
-          <div className="w-[296px] h-[60px] flex items-center gap-[24px] px-[8px] w-full">
+          <div className="w-full h-[60px] flex items-center gap-[24px] px-[8px]">
+            {/* 텍스트 */}
             <div className="flex flex-col gap-[4px]">
               <p className="font-body text-body-1 text-black">
                 마음의 상태가 바뀌었나요?
               </p>
-              <p className="font-body text-detail-4 text-gray-100">
+              <p className="whitespace-nowrap font-body text-detail-4 text-gray-100">
                 가장 마지막으로 진단받은 날짜는 7일 전이에요
               </p>
             </div>
+            {/* 티백 이미지 */}
             <img src={teatImg} alt="Tea" className="w-[60px] h-[60px]" />
           </div>
-          <div>버튼</div>
+          <div>
+            <ButtonMedium text="새로 진단해보기" />
+          </div>
         </div>
       </div>
     </>
