8ab89fe244dedef2e50e43e90f2e87af29d4c36b
chaeyoon <codbs0722@gmail.com>
2026-01-07 16:28:48 +0900

    #3 [UI] 공간카드 mini UI 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import './App.css';
-import SpaceCardMini from './components/common/SpaceCardMini';
 
 function App() {
-  return (
-    <>
-      <SpaceCardMini />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
