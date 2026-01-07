410e7abc8a2e66897783286b265f05b4a2a2e803
chaeyoon <codbs0722@gmail.com>
2026-01-07 16:36:48 +0900

    #3 [UI] ButtonLarge UI 구현

---
+++ src/components/common/Button/ButtonLarge.tsx
@@ -0,0 +1,11 @@
+const ButtonLarge = () => {
+  return (
+    <div className="w-[335px] h-[74px] rounded-md bg-white shadow-[0_0_9.9px_0_rgba(141,141,141,0.24)]">
+      <p className="text-black-2 font-body text-body-2 py-[28px] px-[77px]">
+        좋은 사람들과의 즐거운 대화
+      </p>
+    </div>
+  );
+};
+
+export default ButtonLarge;
