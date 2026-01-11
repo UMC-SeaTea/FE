8db1dfb3be0e8fc85a5f17588a4144aaea806001
chaeyoon <codbs0722@gmail.com>
2026-01-12 00:47:58 +0900

    #13 [UI] navbar 삽입

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,17 @@
+import NavBar from '../components/common/NavBar';
+import menu from '../assets/menu_black.svg';
+
 const HomePage = () => {
-  return <div>홈페이지</div>;
+  return (
+    <>
+      <NavBar
+        variant="2"
+        text="SeaTea"
+        icon={menu}
+        className="border-b border-[#000]"
+      />
+    </>
+  );
 };
 
 export default HomePage;
