303a325148f99b6b9add472281fc8c1995dc0e60
chaeyoon <codbs0722@gmail.com>
2026-01-12 00:52:27 +0900

    #13 [UI] 홈 테이스팅 노트 삽입

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,23 @@
 import NavBar from '../components/common/NavBar';
 import menu from '../assets/menu_black.svg';
+import HomeTestType from '../components/common/HomeTestType';
 
 const HomePage = () => {
   return (
     <>
-      <NavBar
-        variant="2"
-        text="SeaTea"
-        icon={menu}
-        className="border-b border-[#000]"
-      />
+      <div className="flex flex-col gap-2">
+        <NavBar
+          variant="2"
+          text="SeaTea"
+          icon={menu}
+          className="border-b border-[#000]"
+        />
+        <HomeTestType
+          type="sweet"
+          title="sweet."
+          description="소소한 디저트나 쇼핑 등 자신에게 작은 보상을 주며 스트레스를 풀어요"
+        />
+      </div>
     </>
   );
 };
