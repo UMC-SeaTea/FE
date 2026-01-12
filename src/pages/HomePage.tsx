a24632c3e6038ed31a31e8ef1a6284c65f9a9709
chaeyoon <codbs0722@gmail.com>
2026-01-12 10:58:16 +0900

    #13 [FEAT] 공간카드미니 onClick

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,22 +1,33 @@
 import NavBar from '../components/common/NavBar';
 import menu from '../assets/menu_black.svg';
 import HomeTestType from '../components/common/HomeTestType';
+import SpaceCardMini from '../components/common/SpaceCardMini';
 
 const HomePage = () => {
   return (
     <>
-      <div className="flex flex-col gap-2">
-        <NavBar
-          variant="2"
-          text="SeaTea"
-          icon={menu}
-          className="border-b border-[#000]"
-        />
-        <HomeTestType
-          type="sweet"
-          title="sweet."
-          description="소소한 디저트나 쇼핑 등 자신에게 작은 보상을 주며 스트레스를 풀어요"
-        />
+      <div className="flex flex-col gap-[42px]">
+        <div className="flex flex-col gap-2">
+          <NavBar
+            variant="2"
+            text="SeaTea"
+            icon={menu}
+            className="border-b border-[#000]"
+          />
+          <HomeTestType
+            type="sweet"
+            title="sweet."
+            description="소소한 디저트나 쇼핑 등 자신에게 작은 보상을 주며 스트레스를 풀어요"
+          />
+        </div>
+        <div className="flex flex-col pl-[20px] gap-[29px]">
+          <div className="flex flex-col gap-[10px]">
+            <p className="text-black font-body text-body-title">
+              최근 확인한 공간
+            </p>
+            <SpaceCardMini />
+          </div>
+        </div>
       </div>
     </>
   );
