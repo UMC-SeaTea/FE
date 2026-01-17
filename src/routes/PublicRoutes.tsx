6dda8497030914ed45a328b70c32b76dc40d1ee5
jimiiiy <uxneks123@gmail.com>
2026-01-18 01:41:42 +0900

    #17 [MOD] PublicRoutes 수정

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -3,7 +3,9 @@ import ErrorPage from '../pages/ErrorPage';
 import HomePage from '../pages/HomePage';
 import LoginPage from '../pages/Login/LoginPage';
 import LoginStartPage from '../pages/Login/LoginStartPage';
+import MapDetailPage from '../pages/Map/MapDetailPage';
 import MapPage from '../pages/Map/MapPage';
+import SpaceRecommend from '../pages/SpaceRecommend';
 
 export const publicRoutes = [
   {
@@ -12,12 +14,13 @@ export const publicRoutes = [
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/map', element: <MapPage /> },
+      { path: '/map/:sid', element: <MapDetailPage /> },
       { path: '/explore', element: <></> },
       { path: '/mypage', element: <></> },
       { path: '/diagnosis', element: <></> },
       { path: '/login', element : <LoginPage/>},
       { path: '/loginstart', element: <LoginStartPage/>},
-
+      { path: '/recommend', element: <SpaceRecommend /> },
     ],
   },
 ];
