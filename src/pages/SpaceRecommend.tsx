dfa0fdf7d3cc8685e08f5f3c1e010e41b84f5b6f
chaeyoon <codbs0722@gmail.com>
2026-01-22 15:31:53 +0900

    [FIX] import 경로 수정

--- src/pages/SpaceRecommend.tsx
+++ src/pages/SpaceRecommend.tsx
@@ -5,8 +5,8 @@ import tea from '../assets/images/teaIcon.png';
 import refresh from '../assets/refresh.svg';
 import PlaceList from '../components/common/PlaceList';
 import { useState } from 'react';
-import FeedbackButton from '../components/common/Feedback/FeedbackButton';
 import { useNavigate } from 'react-router-dom';
+import FeedbackButton from '../components/Feedback/FeedbackButton';
 
 const SpaceRecommend = () => {
   const [feedback, setFeedback] = useState<'good' | 'bad' | null>(null);
