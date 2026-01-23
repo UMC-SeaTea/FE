f75abe26767fe9f007340f42cc424d1d6821bbce
chaeyoon <codbs0722@gmail.com>
2026-01-23 22:09:02 +0900

    #48 [MOD] router 정리

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,4 +2,3 @@ import { createBrowserRouter } from 'react-router-dom';
 import { publicRoutes } from './PublicRoutes';
 
 export const router = createBrowserRouter([...publicRoutes]);
-export default router;
