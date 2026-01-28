7c935b3bb9b0c37dcbcf6ca6ed9e666cbdb25793
chaeyoon <codbs0722@gmail.com>
2026-01-28 16:56:17 +0900

    #54 [FEAT] API 러프하게 구현

---
+++ src/apis/spaces/spaceList.ts
@@ -0,0 +1,6 @@
+// import type { SpaceListResponse } from '../../types/spaces/spaceList';
+
+// export const getSpaceList = async (): Promise<SpaceListResponse> => {
+//   const res = await publicInstance.get<SpaceListResponse>('/api/v1/spaces');
+//   return res.data;
+// };
---
+++ src/hooks/spaces/useSpaceList.ts
@@ -0,0 +1,7 @@
+// import type { SpaceListResponse } from '../../types/spaces/spaceList';
+
+// export const useSpaceList = () => {
+//   return useMutation<SpaceListResponse, Error>({
+//     mutationFn: getSpaceList,
+//   });
+// };
