63cb43f85e7efb979c6eec1985d67a22d7e6194d
chaeyoon <codbs0722@gmail.com>
2026-01-29 12:17:58 +0900

    #58 [FEAT] ScrollTop 컴포넌트

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import ScrollTop from '../components/common/ScrollTop';
 
 const MainLayout: React.FC = () => {
   return (
     <>
       <div className="w-full max-w-[375px] mx-auto min-h-screen bg-white">
+        <ScrollTop />
         <Outlet />
       </div>
     </>
