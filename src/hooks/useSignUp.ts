977f3e13b1ad3a2bed7fb1d822eada3a7106fc43
jimiiiy <uxneks123@gmail.com>
2026-01-29 12:21:06 +0900

    #55 [MOD] 비밀번호 유효성 검사에 특수문자 추가

--- src/hooks/useSignUp.ts
+++ src/hooks/useSignUp.ts
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 
 export const useSignUp = () => {
   const navigate = useNavigate();
-  
+
   const fileInputRef = useRef<HTMLInputElement>(null);
   const cameraInputRef = useRef<HTMLInputElement>(null);
 
@@ -13,36 +13,40 @@ export const useSignUp = () => {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [nickname, setNickname] = useState('');
-  
-  const [isEmailChecked, setIsEmailChecked] = useState(false); 
+
+  const [isEmailChecked, setIsEmailChecked] = useState(false);
   const [showImgOption, setShowImgOption] = useState(false);
-  
+
   const [profileImage, setProfileImage] = useState<File | null>(null);
   const [previewUrl, setPreviewUrl] = useState<string | null>(null);
 
   const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-  const isPwValid = /^(?=.*[a-zA-Z])(?=.*\d).{8,}$/.test(password);
-  const isPwMatch = password === confirmPassword && confirmPassword.length > 0; 
-  const isStep1Complete = isEmailValid && isEmailChecked && isPwValid && isPwMatch;
+  const isPwValid =
+    /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+|~={}\[\]:;<>?,./]).{8,}$/.test(
+      password
+    );
+  const isPwMatch = password === confirmPassword && confirmPassword.length > 0;
+  const isStep1Complete =
+    isEmailValid && isEmailChecked && isPwValid && isPwMatch;
 
   const getTitle = () => {
-    if (step === 1) return "회원가입";
-    if (step === 2) return "프로필";
-    return "사용자 닉네임";
+    if (step === 1) return '회원가입';
+    if (step === 2) return '프로필';
+    return '사용자 닉네임';
   };
 
   const handleBack = () => {
     if (showImgOption) {
-        setShowImgOption(false);
-        return;
+      setShowImgOption(false);
+      return;
     }
     if (step === 1) navigate(-1);
     else setStep(step - 1);
   };
 
   const handleCheckEmail = () => {
-    if (!isEmailValid) return alert("이메일 형식을 확인해주세요.");
-    alert("사용 가능한 이메일입니다.");
+    if (!isEmailValid) return alert('이메일 형식을 확인해주세요.');
+    alert('사용 가능한 이메일입니다.');
     setIsEmailChecked(true);
   };
 
@@ -57,13 +61,13 @@ export const useSignUp = () => {
       setProfileImage(file);
       setPreviewUrl(URL.createObjectURL(file));
       setShowImgOption(false);
-      setStep(3); 
+      setStep(3);
     }
   };
 
   const handleSubmit = () => {
-    console.log("최종 데이터:", { email, password, nickname, profileImage });
-    alert("회원가입 완료!");
+    console.log('최종 데이터:', { email, password, nickname, profileImage });
+    alert('회원가입 완료!');
     navigate('/');
   };
 
@@ -102,6 +106,6 @@ export const useSignUp = () => {
       handleEmailChange,
       handleImageUpload,
       handleSubmit,
-    }
+    },
   };
 };
