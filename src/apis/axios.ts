a36193a85fc3e72d5fd6fdc404466eff5d778a7f
chaeyoon <codbs0722@gmail.com>
2026-02-05 16:38:15 +0900

    #64 [FEAT] 인터셉터 기반 토큰 로직 구현

---
+++ src/apis/axios.ts
@@ -0,0 +1,95 @@
+import type { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
+import axios from 'axios';
+import { LOCAL_STORAGE_KEYS } from '../constants/key';
+
+interface CustomInternalAxiosRequestConfig extends InternalAxiosRequestConfig {
+  _retry?: boolean;
+}
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+});
+
+export const refreshInstance = (refresh: string): AxiosInstance =>
+  axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    headers: {
+      refreshToken: refresh,
+    },
+  });
+
+// Request Interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // localStorage에서 accessToken을 가져와 요청 헤더에 추가
+    const token = localStorage.getItem(LOCAL_STORAGE_KEYS.accessToken);
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response Interceptor
+axiosInstance.interceptors.response.use(
+  (response) => response, // 성공 시 응답 반환
+  async (error) => {
+    const status = error.response?.status;
+    const originalRequest: CustomInternalAxiosRequestConfig = error.config;
+
+    if (!status) return (console.log(error), Promise.reject(error));
+
+    // 401 에러면서, 아직 재시도 하지 않은 요청 경우 처리
+    if (status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const refreshToken = localStorage.getItem(
+        LOCAL_STORAGE_KEYS.refreshToken
+      );
+
+      // refreshToken이 없으면 로그인 페이지로 이동
+      if (!refreshToken) {
+        localStorage.removeItem(LOCAL_STORAGE_KEYS.accessToken);
+        localStorage.removeItem(LOCAL_STORAGE_KEYS.refreshToken);
+        window.location.href = '/login/start';
+        return Promise.reject(error);
+      }
+
+      // refreshToken으로 새 accessToken 발급
+      try {
+        const { data } =
+          await refreshInstance(refreshToken).post('/api/auth/reissue');
+
+        // 새 토큰 변환
+        const newAccessToken = data?.result.accessToken;
+        const newRefreshToken = data?.result.refreshToken;
+
+        if (!newAccessToken || !newRefreshToken)
+          throw new Error('토큰 재발급 실패');
+        localStorage.setItem(LOCAL_STORAGE_KEYS.accessToken, newAccessToken);
+        localStorage.setItem(LOCAL_STORAGE_KEYS.refreshToken, newRefreshToken);
+
+        originalRequest.headers = originalRequest.headers ?? {};
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        return axiosInstance(originalRequest);
+      } catch (error) {
+        // 에러 발생 시 로그인 페이지로 이동
+        localStorage.removeItem(LOCAL_STORAGE_KEYS.accessToken);
+        localStorage.removeItem(LOCAL_STORAGE_KEYS.refreshToken);
+        window.location.href = '/login/start';
+        return Promise.reject(error);
+      }
+    }
+
+    // 403 에러, 접근 권한이 없는 경우
+    if (status === 403 && !originalRequest._retry) {
+      alert('접근 권한이 없습니다. 관리자에게 문의하세요.');
+      window.location.href = '/';
+      return Promise.reject(error);
+    }
+
+    return Promise.reject(error);
+  }
+);
