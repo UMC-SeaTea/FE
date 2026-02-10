ebd3092a1df9456995de2cd30871fb1ab47ade79
Hyobee02 <moskau0811@gmail.com>
2026-02-10 15:07:16 +0900

    #75 [MOD] axios 수정

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -7,10 +7,7 @@ interface CustomInternalAxiosRequestConfig extends InternalAxiosRequestConfig {
 }
 
 export const axiosInstance = axios.create({
- baseURL: import.meta.env.DEV
-  ? '/api'
-  : import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export const refreshInstance = (refresh: string): AxiosInstance =>
--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,4 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://52.78.134.68:8080',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
