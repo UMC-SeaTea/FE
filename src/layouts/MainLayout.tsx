5f2cab80ef171914d33049fdcbd8dec82e5ee2bc
채윤 <codbs0722@gmail.com>
2026-02-12 22:08:25 +0900

    #114 [FIX] token 있을 때만 fetchProfile하도록 수정

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,13 +2,17 @@ import React, { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import ScrollTop from '../components/common/ScrollTop';
 import { useMemberStore } from '../stores/useMemberStore';
+import { LOCAL_STORAGE_KEYS } from '../constants/key';
 
 const MainLayout: React.FC = () => {
   const fetchProfile = useMemberStore((s) => s.fetchProfile);
   const profile = useMemberStore((s) => s.profile);
 
   useEffect(() => {
-    if (!profile) fetchProfile();
+    const token = localStorage.getItem(LOCAL_STORAGE_KEYS.accessToken);
+    if (!token) return;
+    if (profile) return;
+    fetchProfile();
   }, [fetchProfile, profile]);
 
   return (
